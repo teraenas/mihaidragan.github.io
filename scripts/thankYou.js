916dc4568b74b897d2358fafed71c82e2d59aa97
Mihai Dragan <mihai.dragan85@gmail.com>
2022-09-06 19:33:48 +0300

    added thank you page

---
+++ scripts/thankYou.js
@@ -0,0 +1,33 @@
+const redirectTimer = document.getElementById('redirect-seconds');
+const manualRedirectButton = document.getElementById('manual-redirect-btn');
+let redirectTime = 5;
+redirectTimer.innerText = redirectTime;
+
+const initTheme = () => {
+  let theme = localStorage.getItem('theme');
+  !theme && (theme = 'light');
+  setTheme(theme);
+};
+
+const setTheme = theme => {
+  if (theme === 'light') {
+    document.body.classList.replace('dark', 'light');
+  } else {
+    document.body.classList.replace('light', 'dark');
+  }
+};
+
+addEventListener('DOMContentLoaded', initTheme);
+
+manualRedirectButton.addEventListener('click', () => {
+  location.replace('https://mihaidragan.eu');
+});
+
+const interval = setInterval(() => {
+  if (redirectTime === 0) {
+    clearInterval(interval);
+    return location.replace('https://mihaidragan.eu');
+  }
+  redirectTime--;
+  redirectTimer.innerText = redirectTime;
+}, 1000);
